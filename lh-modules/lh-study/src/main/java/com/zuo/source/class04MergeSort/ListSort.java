4dc03232fab5c38e1af1a0fb9d18f6fd9602b457
lh <992610900@qq.com>
2022-04-28 22:49:03 +0800

    链表的mergesort

--- lh-modules/lh-study/src/main/java/com/zuo/source/class04MergeSort/ListSort.java
+++ lh-modules/lh-study/src/main/java/com/zuo/source/class04MergeSort/ListSort.java
@@ -26,27 +26,72 @@ public class ListSort {
 	}
 	
 	//1.将整个链表对半分为左右
-	
-	public static ListNode divSort(ListNode head) {
+	public ListNode divSort(ListNode head) {
 		if (head.next == null) {
 			return head;
 		}
 		
-		// 开始分半
-		ListNode L = head;
+		// 准备断开的节点
+		ListNode p = head;
+		// 分好的右边节点
 		ListNode R = head;
+		// 临时节点，就是个快指针。
 		ListNode temp = head;
-		while (temp.next !=null) {
-			R = head.next;
+		
+		// 注意这里的边界问题
+		while (temp !=null && temp.next != null) {
+			// R每次移动一个位置，temp动两个，到头后，R就来到了右边分组的头节点。
+			p = R;
+			R = R.next;
+			temp = temp.next.next;
 		}
-		// 一人一半
-		divSort(L);
-		divSort(R);
+		
+		//要断开，数组不需要断开，链表需要断开。
+		p.next = null;
+		ListNode LNode = divSort(head);
+		ListNode RNode = divSort(R);
+		
 		// merge
-		return null;
+		return mergeNode(LNode, RNode);
 	}
 	
-	
-	//2.每个单独排序
-	//3.然后合并
+	public ListNode mergeNode(ListNode L, ListNode R) {
+		
+		if(L == R) {return L;}
+		
+		
+		// 定义一个node，用于返回最后的结果
+		// ListNode resultHead = null;
+		ListNode resNodeHead = new ListNode(1);
+		
+		// 定义辅助节点
+		ListNode temp = resNodeHead;
+		
+		
+		while (L != null && R != null) {
+			if (L.val <= R.val) {
+				// 指针的下个node指向L
+				temp.next = L;
+				// 移动到自己的下一个node！
+				// temp = L;
+				temp = temp.next;
+				L = L.next;
+			} else {
+				temp.next = R;
+				temp = temp.next;
+				R = R.next;
+			}
+		}
+		
+		if (L == null) {
+			temp.next = R;
+		}
+		
+		if (R == null) {
+			temp.next = L;
+		}
+		
+		return resNodeHead.next;
+		
+	}
 }
---
+++ lh-modules/lh-study/src/main/java/com/zuo/source/class04MergeSort/SolutionCountRangeSum.java
@@ -0,0 +1,15 @@
+package com.zuo.source.class04MergeSort;
+/**
+ * @Author lhMeng
+ * @Create 2022-04-28-21:38
+ * @Describe
+ */
+
+/**
+ *@Author: lh
+ *@CreateTime: 2022-04-28  21:38
+ *@Description: 等差数列求和的复杂度 直接 条件反射是 N……
+ *@Version: 1.0
+ */
+public class SolutionCountRangeSum {
+}
